da333d3bb654ed9d005014334f52fae2c821ebaa
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-07-02 17:00:02 -0700

    added countdown

--- script.js
+++ script.js
@@ -1,76 +1,83 @@
 $(document).ready(function(){
 	//grab the value of break length
 	var breakNumber = Number(document.getElementsByClassName('numberBreak')[0].innerHTML);
+
 	//grab the value of session length
 	var sessionNumber = Number(document.getElementsByClassName('numberSession')[0].innerHTML);
-	console.log('current break #: ' + breakNumber);
-	console.log('current session #: ' + sessionNumber);
 
-	// function to increase timer by 1
-	function incrementByOne(number){
-		return number + 1;
+	var totalSecondsRemaining = sessionNumber * 60;
+
+	var isClockPaused = true;
+	
+	displayNewValues();
+
+	function pad(num, size) {
+	    var s = num+"";
+	    while (s.length < size) s = "0" + s;
+	    return s;
 	}
-	// function to decrease timer by 1
-	function decrementByOne(number){
-		return number - 1;
+
+	function displayNewValues () {
+		var minutes = Math.floor(totalSecondsRemaining / 60);
+		var seconds = totalSecondsRemaining % 60;
+		$('.numberBreak').text(breakNumber);
+		$('.numberSession').text(sessionNumber);
+		$('#countDown').text(pad(minutes, 2) + ':' + pad(seconds, 2));
 	}
+
 	// function to reset timer back to default
 	function resetTimer(){
 		breakNumber = 5;
 		sessionNumber = 5;
-		$('.numberBreak').text(breakNumber);
-		$('.numberSession').text(sessionNumber);
+		displayNewValues();
 	}
 	//click handler on break length's add button; increments by 1
 	$('.addBreak').on('click',function(){
-		var incrementedBreakNumber = incrementByOne(breakNumber);
-		breakNumber = incrementedBreakNumber;
-		//change current number to new incremented break number
-		$('.numberBreak').text(breakNumber);
-
-		console.log('incremented break #: ');
-		console.log(breakNumber);
+		breakNumber = breakNumber + 1;	
+		displayNewValues();
 	});
 	//click handler on session length's add button; increments by 1
 	$('.addSession').on('click',function(){
-		var incrementedSessionNumber = incrementByOne(sessionNumber);
-		sessionNumber = incrementedSessionNumber;
-		//change current number to new incremented session number
-		$('.numberSession').text(sessionNumber);
-
-		console.log('incremented session #: ');
-		console.log(sessionNumber);
+		sessionNumber += 1;
+		totalSecondsRemaining = sessionNumber * 60;
+		displayNewValues()
 	});
 
 	//click handler on break length's subtract button; decrements by 1
 	$('.subtractBreak').on('click',function(){
-		if(breakNumber !== 0){
-			//prevents break number from decrementing passed 0
-			var decrementedBreakNumber = decrementByOne(breakNumber);
-			breakNumber = decrementedBreakNumber;
-			$('.numberBreak').text(breakNumber);
-
-			console.log('decremented break #: ');
-			console.log(breakNumber);
+		if(breakNumber == 1){
+			return;
 		}
+		breakNumber = breakNumber - 1;
+		displayNewValues();
 	});
 	//click handler on break length's subtract button; decrements by 1
 	$('.subtractSession').on('click',function(){
-		//prevents session number from decrementing passed 0
-		if(sessionNumber !== 0){
-			var decrementedSessionNumber = decrementByOne(sessionNumber);
-			sessionNumber = decrementedSessionNumber;
-			$('.numberSession').text(sessionNumber);
-
-			console.log('decremented session #: ');
-			console.log(sessionNumber);
+		if(sessionNumber == 1){
+			return;
 		}
+		sessionNumber -= 1;
+		totalSecondsRemaining = sessionNumber * 60;
+		displayNewValues()
 	});
 
 	//reset click handler to set clock back to default settings
-	$('#reset').on('click',function(){
-		resetTimer();
-		console.log('reset to default break #: ' + breakNumber);
-		console.log('reset to default session #: ' + sessionNumber);
+	$('#reset').on('click',resetTimer);
+
+	var intervalId;
+	$('#clock').on('click', function () {
+
+		if(isClockPaused) {
+			isClockPaused = false;
+			intervalId = setInterval(function () {
+				totalSecondsRemaining -= 1;
+				displayNewValues();
+			}, 1000);
+		} else {
+			// clear interval by id
+			clearInterval(intervalId);
+			// set isClockedPaused to true
+			isClockPaused = true;
+		}
 	});
 });
